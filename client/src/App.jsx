3f1cab0798f1ff2e84bdc949a719d046e87637fd
Kenieboy <syntax0330@gmail.com>
2024-02-14 00:02:55 +0800

    table error solved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import DynamicTable from "./components/cutom-components/DynamicTable";
 
 function App() {
   const [data, setData] = useState([]);
@@ -39,73 +19,10 @@ function App() {
   }, []);
 
   console.log(data);
+
   return (
     <div>
-      <div className=" h-screen ">
-        {/* dialog area */}
-
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="outline">Edit Profile</Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Edit profile</DialogTitle>
-              <DialogDescription>
-                Make changes to your profile here. Click save when you're done.
-              </DialogDescription>
-            </DialogHeader>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="name" className="text-right">
-                  Name
-                </Label>
-                <Input id="name" value="" className="col-span-3" />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right">
-                  Username
-                </Label>
-                <Input id="username" value="" className="col-span-3" />
-              </div>
-            </div>
-            <DialogFooter>
-              <Button type="submit">Save changes</Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-        {/* <div className="flex gap-4">
-          {data?.map((item) => (
-            <div className="bg-white p-4" key={item.id}>
-              <div>
-                <h1 className="text-3xl">{item.username}</h1>
-                <p>{item.email}</p>
-                <p>{item.address}</p>
-              </div>
-            </div>
-          ))}
-        </div> */}
-        <Table className="w-1/2">
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">ID</TableHead>
-              <TableHead>USERNAME</TableHead>
-              <TableHead>EMAIL</TableHead>
-              <TableHead className="text-right">ADDRESS</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {data.map((item) => (
-              <TableRow key={item.id}>
-                <TableCell className="font-medium">{item.id}</TableCell>
-                <TableCell>{item.username}</TableCell>
-                <TableCell>{item.email}</TableCell>
-                <TableCell className="text-right">{item.address}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
+      <DynamicTable data={data} />
     </div>
   );
 }
---
+++ client/src/components/cutom-components/DynamicTable.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+function DynamicTable({ data }) {
+  if (!data || data.length === 0) {
+    return (
+      <AlertDialog open={true}>
+        <AlertDialogTrigger>Open</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    ); // Render a message if data is empty or undefined
+  }
+  const columnNames = Object.keys(data[0]);
+
+  return (
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          {columnNames.map((columnName) => (
+            <TableHead key={columnName}>{columnName.toUpperCase()}</TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((rowData, i) => (
+          <TableRow key={i}>
+            {columnNames.map((columnName) => (
+              <TableCell key={columnName}>{rowData[columnName]}</TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
+
+export default DynamicTable;
